8c5aa0f910c4257a0e09918d7747d7aa33056d26
cultureDesSauvages <culture.des.sauvages@gmail.com>
2024-06-14 11:05:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ app.get("/", (req, res) => {
   res.send("Hello Vercel!");
 });
 app.get("/toto", (req, res) => {
+
+fetch("https://google.com")
+  .then((response) => {
+    res.send(response);
+  })
+  .catch(function (err) {
+    res.send("Unable to fetch -");
+  });
+  
+ 
   res.send("Hello toto!");
 });
 

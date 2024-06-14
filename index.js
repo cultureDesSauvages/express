e7e7a5d1b942bedae58891cc0064414922898f79
cultureDesSauvages <culture.des.sauvages@gmail.com>
2024-06-14 10:46:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,5 +7,8 @@ const port = process.env.PORT || 8080;
 app.get("/", (req, res) => {
   res.send("Hello Vercel!");
 });
+app.get("/toto", (req, res) => {
+  res.send("Hello toto!");
+});
 
 app.listen(port);
